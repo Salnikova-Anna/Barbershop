06041254a076bab3fded5fdf50bef08ecdb24fcf
Salnikova-Anna <anna_val@ukr.net>
2023-08-09 20:44:56 +0300

    edit burger-menu

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,16 +1,24 @@
-const bgMenuOpenBtn = document.querySelector(".burger-menu-open-button");
-const bgMenuCloseBtn = document.querySelector(".burger-menu-close-btn");
-const burgerMenu = document.querySelector(".burger-menu");
+const refs = {
+  bgMenuOpenBtn: document.querySelector(".burger-menu-open-button"),
+  bgMenuCloseBtn: document.querySelector(".burger-menu-close-btn"),
+  burgerMenu: document.querySelector(".burger-menu"),
+  navLinks: document.querySelectorAll(".burger-menu-soc-nav-link"),
+};
 
-bgMenuOpenBtn.addEventListener("click", handleOpenMenuClick);
-bgMenuCloseBtn.addEventListener("click", handleCloseMenuClick);
+[...refs.navLinks].map((link) =>
+  link.addEventListener("click", handleCloseMenuClick)
+);
+
+refs.bgMenuOpenBtn.addEventListener("click", handleOpenMenuClick);
+refs.bgMenuCloseBtn.addEventListener("click", handleCloseMenuClick);
 
 function handleOpenMenuClick() {
-  burgerMenu.classList.remove("is-hidden");
+  refs.burgerMenu.classList.add("is-shown");
   document.body.classList.add("no-scroll");
+  // document.body.addEventListener("click", handleCloseMenuClick);
 }
 
 function handleCloseMenuClick() {
-  burgerMenu.classList.add("is-hidden");
+  refs.burgerMenu.classList.remove("is-shown");
   document.body.classList.remove("no-scroll");
 }
