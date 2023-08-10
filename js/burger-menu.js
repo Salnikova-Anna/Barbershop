7cf1985a4ed52e6c6adfdf82128d76e351ca4d97
Salnikova-Anna <anna_val@ukr.net>
2023-08-10 12:14:52 +0300

    add animation to hero section

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -3,6 +3,7 @@ const refs = {
   bgMenuCloseBtn: document.querySelector(".burger-menu-close-btn"),
   burgerMenu: document.querySelector(".burger-menu"),
   navLinks: document.querySelectorAll(".burger-menu-soc-nav-link"),
+  BgMenuBookLink: document.querySelector(".burger-menu-booking-link"),
 };
 
 [...refs.navLinks].map((link) =>
@@ -11,11 +12,11 @@ const refs = {
 
 refs.bgMenuOpenBtn.addEventListener("click", handleOpenMenuClick);
 refs.bgMenuCloseBtn.addEventListener("click", handleCloseMenuClick);
+refs.BgMenuBookLink.addEventListener("click", handleCloseMenuClick);
 
 function handleOpenMenuClick() {
   refs.burgerMenu.classList.add("is-shown");
   document.body.classList.add("no-scroll");
-  // document.body.addEventListener("click", handleCloseMenuClick);
 }
 
 function handleCloseMenuClick() {
